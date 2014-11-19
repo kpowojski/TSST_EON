54a18f9b46cf5598f1d41b5661faf61441217909
kpowojski <krystian.powojski@hotmail.com>
2014-11-19 19:24:25 +0100

    Initializing commit

---
+++ NetworkManager/NetworkManager/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetworkManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
