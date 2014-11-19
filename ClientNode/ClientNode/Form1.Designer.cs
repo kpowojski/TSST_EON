3357ba378f87f8737146eebc3125097406d36c4d
Tomasz93 <tomasz.najda@hotmail.com>
2014-11-19 20:04:25 +0100

    client form

--- ClientNode/ClientNode/Form1.Designer.cs
+++ ClientNode/ClientNode/Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.configButton = new System.Windows.Forms.Button();
+            this.chatListBox = new System.Windows.Forms.ListBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // configButton
+            // 
+            this.configButton.Location = new System.Drawing.Point(12, 12);
+            this.configButton.Name = "configButton";
+            this.configButton.Size = new System.Drawing.Size(129, 23);
+            this.configButton.TabIndex = 0;
+            this.configButton.Text = "Load Configuration";
+            this.configButton.UseVisualStyleBackColor = true;
+            // 
+            // chatListBox
+            // 
+            this.chatListBox.FormattingEnabled = true;
+            this.chatListBox.Location = new System.Drawing.Point(12, 44);
+            this.chatListBox.Name = "chatListBox";
+            this.chatListBox.Size = new System.Drawing.Size(368, 264);
+            this.chatListBox.TabIndex = 1;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(12, 316);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(205, 20);
+            this.messageTextBox.TabIndex = 2;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(305, 314);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(224, 314);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(392, 349);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.chatListBox);
+            this.Controls.Add(this.configButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ClientNode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button configButton;
+        private System.Windows.Forms.ListBox chatListBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button sendButton;
     }
 }
 
