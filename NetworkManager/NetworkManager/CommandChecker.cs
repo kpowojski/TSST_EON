4b0663c151be1191c893a930e85c3cde2de0f8dd
Tomasz93 <tomasz.najda@hotmail.com>
2014-11-24 18:49:50 +0100

    NetworkManager

---
+++ NetworkManager/NetworkManager/CommandChecker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkManager
+{
+    class CommandChecker
+    {
+
+        string error_msg = "";
+
+        public string getErrorMsg()
+        {
+            return error_msg;
+        }
+
+        public Boolean checkCommand(string command)
+        {
+            error_msg = "";
+            string[] words = command.Split(' ');
+            if (words[0].Equals("GETALLNAMES"))
+            {
+                if (words.Length == 1)
+                    return true;
+                else
+                {
+                    error_msg = "Sytnax Error: Incorect list of parameters. Correct syntax: 'GETALL'";
+                    return false;
+                }
+            }
+            else if (words[0].Equals("GET"))
+            {
+                if (words.Length == 2)
+                    return true;
+                else
+                {
+                    error_msg = "Sytnax Error: Incorect list of parameters. Correct syntax: 'GET NODE_NAME'";
+                    return false;
+                }
+            }
+            else if (words[0].Equals("SET"))
+            {
+                if (words.Length == 4)
+                    return true;
+                else
+                {
+                    error_msg = "Sytnax Error: Incorect list of parameters. Correct syntax: 'SET NODE_NAME PORT_IN PORT_OUT'";
+                    return false;
+                }
+            }
+            else if (words[0].Equals("DELETE"))
+            {
+                if (words.Length == 4)
+                    return true;
+                else
+                {
+                    error_msg = "Sytnax Error: Incorect list of parameters. Correct syntax: 'DELETE NODE_NAME PORT_IN PORT_OUT'";
+                    return false;
+                }
+            }
+            else
+            {
+                error_msg = "Unknown command!";
+                return false;
+            }
+        }
+    }
+}
