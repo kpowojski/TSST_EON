4e6b1b18902d0146d07c01415fbddd0a6a8b71fc
kpowojski <krystian.powojski@hotmail.com>
2014-11-24 18:36:28 +0100

    Adding checker to networkNode

---
+++ NetworkNode/NetworkNode/Checker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkNode
+{
+    class Checker
+    {
+        private string nodeId;
+        private List<string> portIn;
+
+
+        public Checker(string nodeId, List<string> portIn)
+        {
+            this.nodeId = nodeId;
+            this.portIn = portIn;
+        }
+
+        public string checkDestination(string message)
+        {
+            
+            string[] words = message.Split(' ');
+            string dstId = words[0];
+            Console.WriteLine(dstId);
+            string dstPortId = words[1];
+            Console.WriteLine(dstPortId);
+            bool destitantionReached = false;
+
+            if (this.nodeId == dstId)
+            {
+                if (this.portIn.Contains(dstPortId))
+                {
+                    destitantionReached = true;
+                }
+            }
+
+            if (destitantionReached == true)
+            {
+                string originalMessage = null;
+                for (int i = 2; i < words.Length; i++)
+                {
+                    originalMessage += " " + words[i];
+                }
+                message = null;
+                message = originalMessage;
+            }
+            else
+            {
+                message = "null";
+            }
+
+            Console.WriteLine(message);
+            return message;
+        }
+
+    }
+}
