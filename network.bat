b476afc283d5c06bb4170bc8cb496c31ef4425d6
kpowojski <krystian.powojski@hotmail.com>
2014-12-01 00:42:01 +0100

    Adding bat file to start our emulator

---
+++ network.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Podaj ile komponentow potrzebujesz:
+
+echo ClientNode
+set/p "ClientNode=>>"
+echo NetworkNode
+set/p "NetworkNode=>>"
+
+
+for /L %%A in (1,1,%ClientNode%) do (
+	START ClientNode\ClientNode\bin\Debug\ClientNode.exe
+	)
+
+for /L %%A in (1,1,%NetworkNode%) do (
+	START NetworkNode\NetworkNode\bin\Debug\NetworkNode.exe
+	)
+
+START NetworkCloud\NetworkCloud\bin\Debug\NetworkCloud.exe
+START NetworkManager\NetworkManager\bin\Debug\NetworkManager.exe
+START NetworkModelViewer\NetworkModelViewer\bin\Debug\NetworkModelViewer.exe
+
+
+pause>nul
