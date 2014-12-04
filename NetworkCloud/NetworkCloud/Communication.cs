d11d0dcb4205b50f7be4ff79247823be8ad9995d
Tomasz93 <tomasz.najda@hotmail.com>
2014-12-04 20:49:26 +0100

    TCP update2

---
+++ NetworkCloud/NetworkCloud/Communication.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Windows.Forms;
+
+namespace NetworkCloud
+{
+    class Communication
+    {
+        private ASCIIEncoding encoder;
+        private Logs logs;
+        private Forwarder forwarder;
+
+        private TcpListener serverSocket;
+        private Thread serverThread;
+        private Dictionary<TcpClient, string> clientSockets = new Dictionary<TcpClient, string>();
+
+        public Communication(Logs logs, Forwarder forwarder)
+        {
+            this.encoder = new ASCIIEncoding();
+            this.logs = logs;
+        }
+
+        //Rozpoczyna pracę serwera
+        public bool startServer(int port)
+        {
+            if (serverSocket == null && serverThread == null)
+            {
+                this.serverSocket = new TcpListener(IPAddress.Any, port);
+                this.serverThread = new Thread(new ThreadStart(ListenForClients));
+                this.serverThread.Start(); 
+                logs.addLog(Constants.CLOUD_STARTED_CORRECTLY, true, Constants.INFO);
+                return true;
+            }
+            else
+            {
+                logs.addLog(Constants.CLOUD_STARTED_ERROR, true, Constants.ERROR);
+                return false;
+            }
+        }
+
+        //Kończy pracę serwera
+        public void stopServer()
+        {
+            foreach (TcpClient clientSocket in clientSockets.Keys.ToList())
+            {
+                clientSocket.GetStream().Close();
+                clientSocket.Close();
+                clientSockets.Remove(clientSocket);
+            }
+            if (serverSocket != null)
+            {
+                serverSocket.Stop();
+            }
+            serverSocket = null;
+            serverThread = null;
+        }
+
+        //Wysyła wiadomość do wybranego klienta
+        public void sendMessage(string name, string msg)
+        {
+            if (serverSocket != null)
+            {
+                NetworkStream stream = null;
+                TcpClient client = null;
+                List<TcpClient> clientsList = clientSockets.Keys.ToList();
+                Console.Write(clientsList.Count);
+                for (int i = 0; i < clientsList.Count; i++)
+                {
+                    if (clientSockets[clientsList[i]].Equals(name))
+                    {
+                        client = clientsList[i];
+                        break;
+                    }
+                }
+
+                if (client != null)
+                {
+                    if (client.Connected)
+                    {
+                        stream = client.GetStream();
+                        byte[] buffer = encoder.GetBytes(msg);
+                        stream.Write(buffer, 0, buffer.Length);
+                        stream.Flush();
+                        logs.addLog(Constants.SENT_MSG + msg, true, Constants.TEXT);
+                    }
+                    else
+                    {
+                        stream.Close();
+                        clientSockets.Remove(client);
+                        logs.addLog(Constants.NODE_UNREACHABLE, true, Constants.ERROR);
+                    }
+                }
+                else
+                {
+                    logs.addLog(Constants.NODE_NOT_CONNECTED, true, Constants.ERROR);
+                }
+            }
+        }
+
+        //Wysyła wiadomość do wszystkich klientów
+        public void sendMessageToAll(string msg)
+        {
+            if (serverSocket != null)
+            {
+                NetworkStream stream;
+                foreach (TcpClient client in clientSockets.Keys.ToList())
+                {
+                    if (client.Connected)
+                    {
+                        stream = client.GetStream();
+                        byte[] buffer = encoder.GetBytes(msg);
+                        stream.Write(buffer, 0, buffer.Length);
+                        stream.Flush();
+                    }
+                }
+                logs.addLog(Constants.SENT_MSG + msg, true, Constants.TEXT);
+            }
+        }
+
+        //Oczekiwanie na połączenie nowych klientów
+        private void ListenForClients()
+        {
+            this.serverSocket.Start();
+            while (true)
+            {
+                try
+                {
+                    TcpClient clientSocket = this.serverSocket.AcceptTcpClient();
+                    clientSockets.Add(clientSocket, "Unknown");
+                    Thread clientThread = new Thread(new ParameterizedThreadStart(displayMessageReceived));
+                    clientThread.Start(clientSocket);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        //Wyświetlanie wiadomości (automatyczne po połączeniu z serwerem)
+        private void displayMessageReceived(object client)
+        {
+            TcpClient clientSocket = (TcpClient)client;
+            NetworkStream stream = clientSocket.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (stream.CanRead)
+            {
+                bytesRead = 0;
+                try
+                {
+                    bytesRead = stream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    break;
+                }
+
+                if (bytesRead == 0) break;
+
+                string str = encoder.GetString(message, 0, bytesRead);
+                if (clientSockets[clientSocket].Equals("Unknown"))
+                {
+                    if (!getClientName(clientSocket, str))
+                    {
+                        logs.addLog(Constants.RECEIVED_MSG + str, true, Constants.TEXT);
+                        string forwardedMessage = forwarder.forwardMessage(str);
+
+                        if (forwardedMessage != null)
+                        {
+                            sendMessageToAll(forwardedMessage);
+                        }
+                    }
+                }
+                else
+                {
+                    logs.addLog(Constants.RECEIVED_MSG + str, true, Constants.TEXT);
+                    string forwardedMessage = forwarder.forwardMessage(str);
+
+                    if (forwardedMessage != null)
+                    {
+                        sendMessageToAll(forwardedMessage);
+                    }
+                }
+            }
+            if (serverSocket != null)
+            {
+                clientSocket.GetStream().Close();
+                clientSocket.Close();
+                clientSockets.Remove(clientSocket);
+                logs.addLog(Constants.DISCONNECTED_NODE, true, Constants.ERROR);
+            }
+
+        }
+
+        //Pobiera z wiadomości nazwę klienta
+        private bool getClientName(TcpClient client, string msg)
+        {
+            if (msg.Contains("//NAME// "))
+            {
+                string[] tmp = msg.Split(' ');
+                clientSockets[client] = tmp[1];
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ NetworkNode/NetworkNode/ManagmentAgent.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NetworkNode
+{
+    class ManagmentAgent
+    {
+        private ASCIIEncoding encoder;
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread clientThread;
+        private string myName;
+        private Logs logs;
+        private Checker checker;
+
+        public ManagmentAgent(string name, Logs logs, Checker checker)
+        {
+            this.encoder = new ASCIIEncoding();
+            this.myName = name;
+            this.logs = logs;
+            this.checker = checker;
+        }
+
+        //Łączenie z serwerem
+        public bool connectToManager(string ip, int port)
+        {
+            if (client == null)
+            {
+                client = new TcpClient();
+                IPAddress ipAddress;
+                if (ip.Contains("localhost"))
+                {
+                    ipAddress = IPAddress.Loopback;
+                }
+                else
+                {
+                    ipAddress = IPAddress.Parse(ip);
+                }
+                try
+                {
+                    client.Connect(new IPEndPoint(ipAddress, port));
+                }
+                catch { }
+                if (client.Connected)
+                {
+                    stream = client.GetStream();
+                    clientThread = new Thread(new ThreadStart(displayMessageReceived));
+                    clientThread.Start();
+                    sendMyName();
+                    logs.addLog(Constants.CONNECTION_MANAGER_SUCCESSFULL, true, Constants.INFO);
+                    return true;
+                }
+                else
+                {
+                    client = null;
+                    logs.addLog(Constants.CONNECTION_MANAGER_ERROR, true, Constants.ERROR);
+                    return false;
+                }
+            }
+            else
+            {
+                logs.addLog(Constants.CONNECTION_MANAGER_CONNECTED_ALREADY, true, Constants.ERROR);
+                return false;
+            }
+        }
+
+        //Kończy połączenie z serwerem
+        public void disconnectFromManager()
+        {
+            if (client != null)
+            {
+                client.GetStream().Close();
+                client.Close();
+                client = null;
+                logs.addLog(Constants.DISCONNECTED_FROM_MANAGEMENT, true, Constants.ERROR);
+            }
+        }
+
+        //Wysyłanie wiadomości
+        public void sendMessage(string msg)
+        {
+            if (client != null && client.Connected)
+            {
+                byte[] buffer = encoder.GetBytes(msg);
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+            }
+        }
+
+        //Wyświetlanie wiadomości (automatyczne po połączeniu z serwerem)
+        private void displayMessageReceived()
+        {
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (stream.CanRead)
+            {
+                bytesRead = 0;
+                try
+                {
+                    bytesRead = stream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    break;
+                }
+
+                if (bytesRead == 0) break;
+
+                string str = encoder.GetString(message, 0, bytesRead);
+
+                string[] response = this.checker.checkManagerCommand(str);
+                if (response != null)
+                {
+                    logs.addLog(Constants.MANAGER_MSG + response[0], true, Constants.RECEIVED);
+                    for (int i = 1; i < response.Length; i++)
+                    {
+                        if (response[i] != "null" && response[i] != null)
+                            sendMessage(response[i]);
+                    }
+                }
+            }
+            if (client != null)
+            {
+                logs.addLog(Constants.NETWORK_MANAGER_DISCONNECTED, true, Constants.ERROR);
+                disconnectFromManager();
+            }
+        }
+
+        //Wysyła serwerowi swoją nazwę, aby mógł zidentyfikować tego klienta
+        private void sendMyName()
+        {
+            byte[] buffer = encoder.GetBytes("//NAME// " + myName);
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Flush();
+        }
+    }
+}
